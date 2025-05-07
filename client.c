bb55dc961c904f8bee52b68a00029bd6b2fb3702
Mahdi Toumi <152063136+Mahdi-toumi@users.noreply.github.com>
2025-05-07 19:52:53 +0100

    Update client.c

--- client.c
+++ client.c
@@ -146,7 +146,7 @@ int main(int argc, char *argv[]) {
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-    if (inet_pton(AF_INET, "192.168.1.178", &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
         perror("Invalid server IP address");
         close(sockfd);
         return 1;
